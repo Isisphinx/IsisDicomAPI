1f9451a9a878ffe64687e0a7cc9dda9987828340
Tetraib <thibaut.constant@isisphinx.com>
2018-06-12 23:58:10 +0200

    Add eslint and review comments

--- routes/route.js
+++ routes/route.js
@@ -1,16 +1,19 @@
-const Router = require('koa-router');
-const router = new Router();
-const {spawn} = require('child_process');
-const {dumpFileFormat, writeFile, dumpFileName} = require('../src/createFile');
+// TODO IDéplacer route.js dans src
+
+const Router = require('koa-router')
+
+const router = new Router()
+const { spawn } = require('child_process')
+const { dumpFileFormat, writeFile, dumpFileName } = require('../src/createFile')
 
 router.put('/v2/Destinations/:Server/Patients/:Patient', (ctx, next) => {
-  ctx.status = 200;
+  ctx.status = 200 // Status 200 seulement après le suucés de la chaine de promise
   writeFile(dumpFileName(ctx.params), dumpFileFormat(ctx.params))
     .then((result) => {
       spawn('dump2dcm/dump2dcm.exe', ['', `Patient${ctx.params.Patient}.dump`, `Patient${ctx.params.Patient}.qry`], { env: { DCMDICTPATH: 'dump2dcm/dicom.dic' } })
     })
-    .catch((err) => { console.log('Erreur : ' + err) })
-  next();
+    .catch((err) => { console.log(`Erreur : ${err}`) })
+  next()
 })
 
 module.exports = router
--- src/createFile.test.js
+++ src/createFile.test.js
@@ -1,6 +1,6 @@
-const {dumpFileFormat, writeFile} = require('./createFile');
-const fs = require('fs');
-const mock = require('mock-fs');
+const { dumpFileFormat, writeFile } = require('./createFile')
+const fs = require('fs')
+const mock = require('mock-fs')
 
 afterEach(() => {
   mock.restore()
@@ -8,17 +8,17 @@ afterEach(() => {
 
 
 test('Should format into dump files', () => {
-  const data = {Patient: 12};
-  expect(dumpFileFormat(data)).toEqual(`(0008,0052) CS [PATIENT]     # QueryRetrieveLevel\n(0010,0020) LO [12]         # PatientID`);
+  const data = { Patient: 12 }
+  expect(dumpFileFormat(data)).toEqual('(0008,0052) CS [PATIENT]     # QueryRetrieveLevel\n(0010,0020) LO [12]         # PatientID')
 })
 
 test('Should create a PatientID.dump', () => {
   expect.assertions(2)
-  const obj = { Patient: 12 };
-  const data = dumpfileFormat(obj);  
+  const obj = { Patient: 12 }
+  const data = dumpfileFormat(obj)
   writeFile(obj, data).then((data) => {
-    expect(data).toEqual('Patient12.dump');
-    expect(fs.readFileSync('Patient12.dump').toEqual(`(0008,0052) CS [PATIENT]     # QueryRetrieveLevel\n(0010,0020) LO [12]         # PatientID`));
+    expect(data).toEqual('Patient12.dump')
+    expect(fs.readFileSync('Patient12.dump').toEqual('(0008,0052) CS [PATIENT]     # QueryRetrieveLevel\n(0010,0020) LO [12]         # PatientID'))
   })
 })
-//test only readFile
+// test only readFile
