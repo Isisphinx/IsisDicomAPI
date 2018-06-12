4b0565e5715d0fbe2c9d57192b56b5cbbc9dd2f2
Marc-Antoine Dupire <marcantoinedupire@gmail.com>
2018-06-12 18:00:39 +0200

    Ajout .then

--- routes/route.js
+++ routes/route.js
@@ -6,10 +6,10 @@ const {dumpFileFormat, writeFile, dumpFileName} = require('../src/createFile');
 router.put('/v2/Destinations/:Server/Patients/:Patient', (ctx, next) => {
   ctx.status = 200;
   writeFile(dumpFileName(ctx.params), dumpFileFormat(ctx.params))
-    // .then((result) => {
-    //   spawn('dump2dcm/dump2dcm.exe', ['', `Patient${ctx.params.Patient}.dump`, `Patient${ctx.params.Patient}.qry`], { env: { DCMDICTPATH: 'dump2dcm/dicom.dic' } })
-    // })
-    // .catch((err) => { console.log('Erreur : ' + err) })
+    .then((result) => {
+      spawn('dump2dcm/dump2dcm.exe', ['', `Patient${ctx.params.Patient}.dump`, `Patient${ctx.params.Patient}.qry`], { env: { DCMDICTPATH: 'dump2dcm/dicom.dic' } })
+    })
+    .catch((err) => { console.log('Erreur : ' + err) })
   next();
 })
 
