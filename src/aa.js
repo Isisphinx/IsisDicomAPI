e2900eaffb80044eddc2b86b78927f3630a751f7
Marc-AntoineD <marcantoinedupire@gmail.com>
2018-06-16 08:13:56 +0200

    File's no needed anymore

--- src/aa.js
+++
@@ -1,9 +0,0 @@
-const { convertDumpToDicomFile } = require('./createFile')
-
-convertDumpToDicomFile('Patient15.dump', 'Patient12.dcm')
-  .then((result) => {
-    console.log(result)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
