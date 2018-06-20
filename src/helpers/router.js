553c8e688d988a72777d1c520690921a38ec4d62
Marc-AntoineD <marcantoinedupire@gmail.com>
2018-06-20 09:54:11 +0200

    Add doc of 'routerFunct'

--- src/helpers/router.js
+++ src/helpers/router.js
@@ -5,6 +5,15 @@ const route = require('path-match')({
   end: false,
 })
 
+/**
+ * This function look if the url and method match the route.
+ * If yes, return the parameters of the request,
+ * otherwise, return false
+ * @param {string} input request methods
+ * @param {string} input url
+ * @param {string} input ctx of koa middleware
+ * @returns parameters of the request or false
+ */
 const routerFunct = (method, url, ctx) => {
   if (method !== ctx.method) return false
   const params = route(url)(ctx.path)
