00ead60d3c6c80b459d8cead91fb3677dd177a32
Marc-AntoineD <marcantoinedupire@gmail.com>
2018-06-20 11:21:46 +0200

    Add test for 'writeFile' & 'exec'

---
+++ src/helpers/promise.test.js
@@ -0,0 +1,32 @@
+const { writeFile, exec } = require('./promise')
+const mock = require('mock-fs')
+const path = require('path')
+
+beforeEach(() => {
+  mock()
+})
+
+afterEach(() => {
+  mock.restore()
+})
+
+it('Test resolve promise of writeFile function', () => {
+  expect.assertions(1)
+  return expect(writeFile('test.txt', 'blabla')).resolves.toEqual(undefined)
+})
+
+it('Test reject promise of writeFile function', () => {
+  expect.assertions(1)
+  return expect(writeFile(123, 'blabla')).rejects.toThrow('EBADF, bad file descriptor')
+})
+
+it('Test resolve promise of exec function', () => {
+  expect.assertions(1)
+  const pathdump2dcm = path.join(__dirname, '..', '..', 'bin', 'dump2dcm', 'dump2dcm.exe')
+  return expect(exec(pathdump2dcm)).resolves.toMatchObject({ stderr: '' }, { stdout: '' })
+})
+
+it('Test reject promise of exec function', () => {
+  expect.assertions(1)
+  return expect(exec('123')).rejects.toThrow('Command failed: 123')
+})
